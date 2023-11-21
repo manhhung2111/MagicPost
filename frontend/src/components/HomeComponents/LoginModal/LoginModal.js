e43f50e6e6bc4b680407a096c2ec80b1eb5ef734
Hoàng Mạnh Hùng <manhhung2720@gmail.com>
2023-11-21 20:06:25 +0700

    Begin to integrate login user and create UI for second feature

--- frontend/src/components/HomeComponents/LoginModal/LoginModal.js
+++ frontend/src/components/HomeComponents/LoginModal/LoginModal.js
@@ -1,7 +1,39 @@
 import Modal from "react-bootstrap/Modal";
 
 import "./LoginModal.scss";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { handleLogin } from "../../../services/authorizationService";
 function LoginModal({ showLoginModal, setShowLoginModal }) {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const validateInputs = () => {
+    if (!userName) {
+      toast.error("User name must be filled");
+      return false;
+    }
+    if (!password) {
+      toast.error("Password must be filled");
+      return false;
+    }
+    return true;
+  };
+
+  const handleSubmit = () => {
+    const result = handleLogin(userName, password);
+    if (result.errorCode === 1) {
+      toast.error("Username or password is wrong. Please try again");
+      return;
+    }
+    if (result.errorCode === 0) {
+      localStorage.setItem(
+        "account",
+        JSON.stringify({ ...result.data, isAuthenticated: true })
+      );
+      toast.success("Login successfully");
+    }
+  };
   return (
     <>
       <Modal
@@ -20,31 +52,27 @@ function LoginModal({ showLoginModal, setShowLoginModal }) {
               <input
                 type="text"
                 className="input-field"
-                // value="Alexander Parkinson"
                 required
-                autoFocus
+                value={userName}
+                onChange={(e) => setUserName(e.target.value)}
               />
-              <label className="input-label">Full name</label>
+              <label className="input-label">Username</label>
             </div>
             <div className="input">
               <input
-                type="text"
+                type="password"
                 className="input-field"
-                // value="vlockn@gmail.com"
                 required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
-              <label className="input-label">Email</label>
-            </div>
-            <div className="input">
-              <input type="password" className="input-field" required />
               <label className="input-label">Password</label>
             </div>
-            <button>Log in</button>
+            <button onClick={handleLogin}>Log in</button>
           </form>
           <div className="card-info">
             <p>
-              By logging in you are agreeing to our{" "}
-              <b>Terms and Conditions</b>
+              By logging in you are agreeing to our <b>Terms and Conditions</b>
             </p>
           </div>
         </Modal.Body>
---
+++ frontend/src/services/authorizationService.js
@@ -0,0 +1,8 @@
+import axios from "../config/axiosConfig";
+
+const handleLogin = async (username, password) => {
+  const result = await axios.post("login", { username, password });
+  return result;
+};
+
+export { handleLogin };
