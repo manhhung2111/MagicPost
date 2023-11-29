65fc4af36f8c90ae10950d329b2b9fd9d9e77367
Hoàng Mạnh Hùng <manhhung2720@gmail.com>
2023-11-30 06:50:59 +0700

    Finish create order for recipient in transaction UI

--- frontend/src/components/ParcelTransactionComponents/RecipientOrderTransaction/RecipientOrderTransaction.js
+++ frontend/src/components/ParcelTransactionComponents/RecipientOrderTransaction/RecipientOrderTransaction.js
@@ -1,6 +1,67 @@
+import Container from "react-bootstrap/Container";
+import "./RecipientOrderTransaction.scss";
+import DelvieryOrderRecipientTable from "./DelvieryOrderRecipientTable";
+import Select from "react-select";
 function RecipientOrderTransaction() {
-    return <h2>Recipient Order Transaction</h2>;
-  }
-  
-  export default RecipientOrderTransaction;
-  
+  const orders = [
+    {
+      parcelId: "MPN0WBQ9JJoHN",
+      address: "Long canh 86, Vinhomes Thang Long",
+      district: "Hoai Duc",
+    },
+    {
+      parcelId: "MPN0WBQ9JJoHN",
+      address: "Long canh 86, Vinhomes Thang Long",
+      district: "Hoai Duc",
+    },
+    {
+      parcelId: "MPN0WBQ9JJoHN",
+      address: "Long canh 86, Vinhomes Thang Long",
+      district: "Hoai Duc",
+    },
+    {
+      parcelId: "MPN0WBQ9JJoHN",
+      address: "Long canh 86, Vinhomes Thang Long",
+      district: "Hoai Duc",
+    },
+  ];
+  const collectionHubs = [
+    { label: "Hoai Duc", value: "Hoai Duc" },
+    { label: "Cau Giay", value: "Cau Giay" },
+  ];
+  const sortBy = [
+    { label: "Alphabetical", value: "Alphabetical" },
+    { label: "Date (asc)", value: "Date (asc)" },
+    { label: "Date (desc)", value: "Date (desc)" },
+  ];
+
+  return (
+    <Container className="recipient-order-transaction-delivery">
+      <h2>Create recipient's delivery order</h2>
+      <div className="filter">
+        <div className="left-content">
+          <p>Select by district</p>
+          <Select
+            defaultValue={[]}
+            isMulti
+            options={collectionHubs}
+            className="multi-select"
+            placeholder={"Select district"}
+          />
+        </div>
+        <div className="right-content">
+          <p>Sort by</p>
+          <Select
+            defaultValue={sortBy[0]}
+            options={sortBy}
+            className="multi-select"
+            placeholder={"Sort by...."}
+          />
+        </div>
+      </div>
+      <DelvieryOrderRecipientTable orders={orders} />
+    </Container>
+  );
+}
+
+export default RecipientOrderTransaction;
