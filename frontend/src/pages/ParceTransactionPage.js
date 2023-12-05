53636642ec05c31de8cac907197a6eb17d8c3da3
Hoàng Mạnh Hùng <manhhung2720@gmail.com>
2023-12-05 20:47:43 +0700

    Chnage parcel transaction page for employee

--- frontend/src/pages/ParceTransactionPage.js
+++ frontend/src/pages/ParceTransactionPage.js
@@ -1,50 +1,75 @@
 import Container from "react-bootstrap/Container";
 import "./Pages.scss";
-import { NavLink, Outlet } from "react-router-dom";
-import { BiPackage } from "react-icons/bi";
-import { LuFileSpreadsheet, LuPackageCheck } from "react-icons/lu";
-import {
-  TbTruckDelivery,
-  TbPackageImport,
-  TbPackageExport,
-} from "react-icons/tb";
-import { IoBarChartOutline } from "react-icons/io5";
+import { FaPlus } from "react-icons/fa6";
+import homeDeliveryWorker from "../assets/loading-workman.jpg";
+import { useState } from "react";
+import SenderOrderTransaction from "../components/ParcelTransactionComponents/SenderOrderTransaction/SenderOrderTransaction";
+import OrderFromTransactionToCollection from "../components/ParcelTransactionComponents/OrderFromTransactionToCollection/OrderFromTransactionToCollection";
+import OrderFromCollectionToTransaction from "../components/ParcelTransactionComponents/OrderFromCollectionToTransaction/OrderFromCollectionToTransaction.js";
+import RecipientOrderTransaction from "../components/ParcelTransactionComponents/RecipientOrderTransaction/RecipientOrderTransaction.js";
+import ParcelTransactionStatusStatistics from "../components/ParcelTransactionComponents/ParcelStatusStatistics/ParcelTransactionStatusStatistics.js";
+import { FaCalendarDays } from "react-icons/fa6";
+
 function ParcelTransactionPage() {
+  const [showAddNewSenderOrderModal, setShowAddNewSenderOrderModal] =
+    useState(false);
+
+  const getGreetingMessage = () => {
+    const today = new Date();
+    const userName =
+      JSON.parse(localStorage.getItem("account")).name ?? "Manh Hung";
+    if (today.getHours() >= 0 && today.getHours() < 12)
+      return `Good morning, ${userName}!`;
+    else if (today.getHours() <= 18) return `Good afternoon, ${userName}!`;
+    return `Good evening, ${userName}!`;
+  };
+
+  const getCurrentDate = () => {
+    const today = new Date();
+    const arr = today.toDateString().split(" ");
+    return `${arr[2]} ${arr[1]}, ${arr[3]}`;
+  };
   return (
     <Container className="parcel-transaction-page">
-      <div className="side-bar">
-        <h3>
-          <BiPackage className="icon" />
-          Magic Post
-        </h3>
-        <NavLink to={""} className="nav-link" end>
-          <LuFileSpreadsheet className="icon" />
-          Create sender's order
-        </NavLink>
-        <NavLink to={"./delivery-order-collection-point"} className="nav-link" end>
-          <TbPackageExport className="icon" />
-          Create order to collection hub
-        </NavLink>
-        <NavLink to={"./confirm-order-collection-point"} className="nav-link">
-          <TbPackageImport className="icon" />
-          Confirm orders from collection hub
-        </NavLink>
-        <NavLink to={"./recipient-order"} className="nav-link">
-          <TbTruckDelivery className="icon" />
-          Create delivery order for recipients
-        </NavLink>
-        <NavLink to={"./confirm-recipient-order"} className="nav-link">
-          <LuPackageCheck className="icon" />
-          Confirm recipients' orders
-        </NavLink>
-        <NavLink to={"./statistics"} className="nav-link">
-          <IoBarChartOutline className="icon" />
-          Parcels' status statistics
-        </NavLink>
+      <div className="sender-order-and-transfer-order">
+        <div className="right-content">
+          <header className="header">
+            <div className="left">
+              <h3>{getGreetingMessage()}</h3>
+              <p>Welcome back, let's begin your work here.</p>
+            </div>
+            <div className="right">
+              <div className="calendar">
+                <FaCalendarDays className="icon" />
+                <p>{getCurrentDate()}</p>
+              </div>
+            </div>
+          </header>
+          <OrderFromTransactionToCollection />
+        </div>
+        <div className="left-content">
+          <img
+            src={homeDeliveryWorker}
+            alt="Home delivery worker"
+            className="img"
+          />
+          <h3>
+            Start your day with a <span>new customer's parcel</span>
+          </h3>
+          <button onClick={() => setShowAddNewSenderOrderModal(true)}>
+            Add a new parcel <FaPlus className="icon" />
+          </button>
+        </div>
       </div>
-      <div className="main-content">
-        <Outlet />
+      <div className="confirm-create-collection-recipient">
+        <OrderFromCollectionToTransaction />
+        <RecipientOrderTransaction />
       </div>
+      <ParcelTransactionStatusStatistics />
+      <SenderOrderTransaction
+        show={showAddNewSenderOrderModal}
+        setShow={setShowAddNewSenderOrderModal}
+      />
     </Container>
   );
 }
