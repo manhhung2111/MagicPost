b842a22a13c0cb63b81641a32072f60ef5815e75
Hoàng Mạnh Hùng <manhhung2720@gmail.com>
2023-12-29 12:14:32 +0700

    finish tracking history

--- backend/src/services/userServices.js
+++ backend/src/services/userServices.js
@@ -2,6 +2,7 @@ import User from "../models/User";
 const jwt = require("jsonwebtoken");
 import Order from "../models/Order";
 import Center from "../models/Center";
+import Shipment from "../models/Shipment";
 import _ from "lodash";
 
 const createToken = (role_name, user_name, center_name) => {
@@ -44,6 +45,17 @@ const loginUser = async (user_name, password) => {
 };
 
 const getParcelById = async (id) => {
+  const getCurrentTime = (d) => {
+    let date = new Date(d);
+    const month = date.toLocaleString("en-US", { month: "long" });
+    const time = date.toLocaleTimeString(["en-US"], {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+    const day = date.getDate();
+    const year = date.getFullYear();
+    return `${time}, ${month} ${day}, ${year}`;
+  };
   try {
     let result = await Order.findOne({ parcelId: id });
     if (!result) {
@@ -53,16 +65,32 @@ const getParcelById = async (id) => {
         message: "Parcel not found",
       };
     }
-    const centers = []
+    const shipment = await Shipment.findOne({ parcelId: id });
+    const date = new Date(result.paths[0].time.timeArrived);
+    const afterDate = getCurrentTime(new Date().setDate(date.getDate() + 30));
+    const status = {
+      timeDelivered: afterDate,
+      status: "In transit",
+    };
+    if (shipment) {
+      if (status.status !== shipment.status) {
+        status.status = shipment.status;
+        status.timeDelivered = shipment.timeDelivered;
+      }
+    }
+
+    const centers = [];
     for (let i = 0; i < result.paths.length; i++) {
       const center = await Center.findOne({
         center_code: result.paths[i].center_code,
       });
-      centers.push(center.name)
+      centers.push(center.name);
     }
+
+    console.log(centers);
     return {
       errorCode: 0,
-      data: {parcel: result, centers},
+      data: { parcel: result, status: { ...status, centers: centers } },
       message: "Parcel is found successfully",
     };
   } catch (error) {
